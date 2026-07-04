665024426370fddd0b0b2fe1863c4f55c443345b
imhvit <yosifblancotafur2006@gmail.com>
2026-07-04 10:03:25 -0500

    feat: agregar archivo app.ts y ssr.ts para configuración de Inertia y Vue SSR style: unificar comillas en app.css

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,14 +1,14 @@
-import { createInertiaApp } from "@inertiajs/vue3";
-import createServer from "@inertiajs/vue3/server";
-import { createSSRApp, h } from "vue";
-import { renderToString } from "vue/server-renderer";
+import { createInertiaApp } from '@inertiajs/vue3';
+import createServer from '@inertiajs/vue3/server';
+import { createSSRApp, h, type DefineComponent } from 'vue';
+import { renderToString } from 'vue/server-renderer';
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
         resolve: (name) => {
-            const pages = import.meta.glob("./pages/**/*.vue");
+            const pages = import.meta.glob<DefineComponent>('./pages/**/*.vue');
             return pages[`./pages/${name}.vue`]();
         },
         setup({ App, props, plugin }) {
